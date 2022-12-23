17171fd4c8d93e6fec31e6d480f7bc6a197dc05e
Aviroop-001 <banerjeeaviroop01@gmail.com>
2022-12-23 11:09:43 +0530

    Added display name to Firebase auth profile

--- components/Authentication/functions.js
+++ components/Authentication/functions.js
@@ -1,5 +1,5 @@
 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc, getDocs, collection } from "firebase/firestore"; 
 import { auth, db } from "../../firbase";
 
@@ -7,12 +7,16 @@ const registerUser = async ({username, email, password}) => {
   createUserWithEmailAndPassword(auth, email, password) //creates new user in auth
     .then(async (userCredential) => {
       const user = userCredential.user;
+      console.log(user)
       try {
         await setDoc(doc(db, "users", user.uid), {  //creating a doc with user details and custom doc id
           username: username,
           email: email,
           likes: [],
         });
+        await updateProfile(user, { displayName: username }).catch(
+          (err) => console.log(err)
+        );
       } catch (e) {
         console.error("Error adding document: ", e);
       }
