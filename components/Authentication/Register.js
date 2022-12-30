c8c893cf05ccc18b272c35a2bd5bce2006fb2cd8
Aviroop-001 <banerjeeaviroop01@gmail.com>
2022-12-30 10:56:08 +0530

    Fixed Register Page inputs

--- components/Authentication/Register.js
+++ components/Authentication/Register.js
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import React, { useState } from "react";
-import { Input, Button } from "@rneui/themed";
+import { Button } from "@rneui/themed";
 import { registerUser } from "./functions";
 
 const Register = ({navigation}) => {
@@ -11,29 +11,21 @@ const Register = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.headerText}>Register</Text> */}
       <View style={styles.main}>
-      <Input
-        label="Username"
-        placeholder="Eg. John Doe"
-        autoFocus={true}
-        onChange={(e) => setusername(e.target.value)}
-        disabledInputStyle={{ background: "#ddd" }}
-        containerStyle={styles.inputContainer}
+      <TextInput
+        style={styles.inputContainer}
+        placeholder="Username"
+        onChangeText={(val) => setusername(val)}
       />
-      <Input
-        label="User Email"
-        placeholder="Eg. JohnDoe@xyz.com"
-        onChange={(e) => setemail(e.target.value)}
-        disabledInputStyle={{ background: "#ddd" }}
-        containerStyle={styles.inputContainer}
+      <TextInput
+        style={styles.inputContainer}
+        placeholder="Email"
+        onChangeText={(val) => setemail(val)}
       />
-      <Input
-        label="Enter Password"
-        placeholder="a-z, A-Z, 0-9"
-        onChange={(e) => setpassword(e.target.value)}
-        secureTextEntry={true}
-        containerStyle={styles.inputContainer}
+      <TextInput
+        style={styles.inputContainer}
+        placeholder="Password"
+        onChangeText={(val) => setpassword(val)}
       />
       <Button title='Register' type="clear" size="md" color= "wheat" buttonStyle={styles.btnStyle} containerStyle={styles.btnContainer} titleStyle={styles.btnTitleStyle}
       onPress={() =>{
@@ -63,8 +55,12 @@ const styles = StyleSheet.create({
     letterSpacing: 2,
     fontWeight: 'bold',
   },
-  inputContainer:{
-    width: 300, height: 40, marginTop: 60,
+  inputContainer: {
+    width: 300,
+    height: 70,
+    marginTop: 60,
+    padding: 10,
+    borderBottomWidth: 1
   },
   btnContainer:{
     width: 200,
